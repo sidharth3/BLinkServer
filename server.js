01dfac7ae6d04dd0f0513eae372a768806ace5eb
Sean Lim <seanmichael_lim@mymail.sutd.edu.sg>
2019-10-30 14:18:28 +0800

    initialisation of firestore

--- server.js
+++ server.js
@@ -17,6 +17,76 @@ getUsers().then((users)=>{
     console.log(users);
 });
 
+/* Prepare different collections and documents, there'll be 4 collections 
+ #1 Users 
+ #2 Event
+ #3 Event Users 
+ #4 Connections */
+
+//Setting up Collection 1: Users, Document: USernames, Field: User/Userid/Email/password/Events attended
+let colRef_users = firestore.collection('users');
+let users_list = ["sean"];
+for (let i of users_list){
+    docRef_users = colRef_users.doc(i);
+    let set_users = docRef_users.set({
+        first_name: '',
+        last_name: '',
+        email:'',
+        password:'',
+        birth_year:''
+    });
+}
+
+console.log("users loaded");
+
+
+//Setting up Collection 2 : Events, Document : UUIDs, Field: Event Name, Date, Organiser UUID, Price
+let colRef_events = firestore.collection('events');
+let events_list = ["123"];
+for (let i of events_list){
+    docRef_events = colRef_events.doc(i);
+    let set_events = docRef_events.set({
+        event_name : '',
+        date: '', //in DDMMYY
+        organiser_id: '',
+        price: '' 
+    }) 
+}
+
+console.log("events loaded");
+
+
+//Setting up collection 3: organisers, Document: UUIDs, Field : USer/User ID/Email/PAssword/ORganisation/Events organised
+let colRef_organisers = firestore.collection('organisers');
+let organisers_list = ["ustd"];
+for (let i of organisers_list){
+    docRef_organisers = colRef_organisers.doc(i);
+    let set_organisers = docRef_organisers.set({
+        first_name : '',
+        last_name :'',
+        email:'',
+        password:'',
+        organisation_name:'',
+        events_organised: '',
+    }) 
+}
+
+console.log("organisers loaded");
+
+//Setting up collection 4: Connection. Document: Random number. Field: User A: USer B
+let colRef_connections = firestore.collection('connections');
+let connections_num = ["0"];
+let userA_list = ["viet"]
+let userB_list = ["jie lin"]
+for (let i=0; i < connections_num.length; i++){
+    docRef_connections = colRef_connections.doc(connections_num[i]);
+    let set_connections = docRef_connections.set({
+        0 : [userA_list[i],userB_list[i]]
+    })
+}
+console.log("connections loaded");
+
+
 
 app.use(bodyParse.json());
 app.use(bodyParse.urlencoded({extended: false }));
@@ -44,4 +114,4 @@ async function getUsers(){
     }
 
     return users;        
-}   
+}
